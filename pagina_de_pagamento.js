1be11e4380985ab5dfaea7ade7166a7b7d193a0e
j4ckson185 <jackson_division@hotmail.com>
2024-07-18 15:11:26 -0300

    Update pagina_de_pagamento.js

--- pagina_de_pagamento.js
+++ pagina_de_pagamento.js
@@ -1,110 +1,92 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Carregar resumo do pedido
-    updateOrderSummary();
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
+    authDomain: "cabana-8d55e.firebaseapp.com",
+    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com",
+    projectId: "cabana-8d55e",
+    storageBucket: "cabana-8d55e.appspot.com",
+    messagingSenderId: "706144237954",
+    appId: "1:706144237954:web:345c10370972486afc779b",
+    measurementId: "G-96Y337GYT8"
+};
 
-    // Função para exibir ou ocultar a seção de troco e mensagem PIX
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Função para exibir ou ocultar a seção de troco
     document.querySelectorAll('input[name="payment-method"]').forEach((radio) => {
         radio.addEventListener('change', function() {
             const trocoSection = document.getElementById('troco-section');
-            const pixInfo = document.getElementById('pix-info');
             if (this.value === 'dinheiro') {
                 trocoSection.style.display = 'block';
-                pixInfo.style.display = 'none';
-            } else if (this.value === 'pix') {
-                pixInfo.style.display = 'block';
-                trocoSection.style.display = 'none';
             } else {
                 trocoSection.style.display = 'none';
-                pixInfo.style.display = 'none';
             }
         });
     });
-
-    // Função para atualizar o resumo do pedido
-    function updateOrderSummary() {
-        const subtotalElement = document.getElementById('subtotal');
-        const totalElement = document.getElementById('total');
-        const carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
-
-        let subtotal = 0;
-        carrinho.forEach(item => {
-            subtotal += parseFloat(item.preco);
-        });
-
-        subtotalElement.textContent = formatarMoeda(subtotal);
-        totalElement.textContent = formatarMoeda(subtotal + 3);
-    }
 
     // Função para formatar o valor como moeda
     function formatarMoeda(valor) {
         return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     }
 
-    // Função para enviar o pedido pelo WhatsApp e salvar no Firestore
-    function enviarPedidoWhatsApp() {
+    // Função para enviar os dados do formulário via WhatsApp e salvar no Firestore
+    document.getElementById('formulario-pagamento').addEventListener('submit', function(event) {
+        event.preventDefault();
         const nomeSobrenome = document.getElementById('nome-sobrenome').value;
-        const cep = document.getElementById('cep').value;
         const endereco = document.getElementById('endereco').value;
-        const numero = document.getElementById('numero').value;
-        const complemento = document.getElementById('complemento').value;
         const telefone = document.getElementById('telefone').value;
+        const email = document.getElementById('email').value;
         const metodoPagamento = document.querySelector('input[name="payment-method"]:checked').value;
         const troco = document.getElementById('troco').value;
+
+        // Obtém os itens do carrinho
         const carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
-
-        let message = `*Pedido Cabana Açaí*\n\n`;
-        message += `*Nome:* ${nomeSobrenome}\n`;
-        message += `*CEP:* ${cep}\n`;
-        message += `*Endereço:* ${endereco}, ${numero}\n`;
-        if (complemento) {
-            message += `*Complemento:* ${complemento}\n`;
-        }
-        message += `*Telefone:* ${telefone}\n\n`;
-
-        message += `*Método de Pagamento:* ${metodoPagamento}\n`;
-        if (metodoPagamento === 'dinheiro' && troco) {
-            message += `*Troco para:* ${troco}\n`;
-        }
-
-        message += `\n*Detalhes do Pedido:*\n`;
+        let mensagemCarrinho = "*Itens do Pedido:*\n";
+        let total = 0;
         carrinho.forEach(item => {
-            let itemDetails = `-${item.nome}: ${formatarMoeda(item.preco)}\n`;
-            if (item.sabores1 && item.sabores2) {
-                itemDetails += `  Sabores (Açaí 1): ${item.sabores1.join(', ')}\n`;
-                itemDetails += `  Sabores (Açaí 2): ${item.sabores2.join(', ')}\n`;
-                itemDetails += `  Acompanhamentos (Açaí 1): ${item.acompanhamentos1.join(', ')}\n`;
-                itemDetails += `  Acompanhamentos (Açaí 2): ${item.acompanhamentos2.join(', ')}\n`;
-                itemDetails += `  Itens Extras (Açaí 1): ${item.extras1.join(', ')}\n`;
-                itemDetails += `  Itens Extras (Açaí 2): ${item.extras2.join(', ')}\n`;
-            } else {
-                itemDetails += `  Sabores: ${item.sabores ? item.sabores.join(', ') : item.sabores1.join(', ')}\n`;
-                itemDetails += `  Acompanhamentos: ${item.acompanhamentos ? item.acompanhamentos.join(', ') : item.acompanhamentos1.join(', ')}\n`;
-                itemDetails += `  Itens Extras: ${item.extras ? item.extras.join(', ') : item.extras1.join(', ')}\n`;
-            }
-            message += itemDetails;
+            let valorProduto = parseFloat(item.preco);
+            total += valorProduto;
+
+            mensagemCarrinho += `\n*${item.nome} - ${formatarMoeda(valorProduto)}*\n`;
+            mensagemCarrinho += "Sabores:\n";
+            item.sabores.forEach(sabor => {
+                mensagemCarrinho += `- ${sabor}\n`;
+            });
+            mensagemCarrinho += "Acompanhamentos:\n";
+            item.acompanhamentos.forEach(acompanhamento => {
+                mensagemCarrinho += `- ${acompanhamento}\n`;
+            });
+            mensagemCarrinho += "Itens Extras:\n";
+            item.extras.forEach(extra => {
+                mensagemCarrinho += `- ${extra}\n`;
+            });
         });
 
-        const subtotalValue = carrinho.reduce((total, item) => total + parseFloat(item.preco), 0);
-        const totalPedido = subtotalValue + 3; // Total do pedido considerando taxa de entrega
-        message += `\n*Taxa de Entrega:* R$ 3,00\n`;
-        message += `*Total do Pedido:* ${formatarMoeda(totalPedido)}`;
+        let mensagem = `*Pedido Cabana Açaí*\n\n*Nome:* ${nomeSobrenome}\n*Endereço:* ${endereco}\n*Telefone:* ${telefone}\n*Email:* ${email}\n*Método de Pagamento:* ${metodoPagamento}\n${mensagemCarrinho}\n*Total do Pedido: ${formatarMoeda(total)}*`;
+        
+        if (metodoPagamento === 'dinheiro' && troco) {
+            mensagem += `\n*Troco para:* ${troco}`;
+        }
 
-        const whatsappLink = `https://wa.me/5584988731028?text=${encodeURIComponent(message)}`;
-        window.open(whatsappLink, '_blank');
+        console.log('Enviando pedido pelo WhatsApp:');
+        console.log(mensagem);
+
+        const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(mensagem)}`;
+        window.open(whatsappUrl, '_blank');
 
         // Salvar o pedido no Firestore
         db.collection("orders").add({
             nomeSobrenome: nomeSobrenome,
-            cep: cep,
             endereco: endereco,
-            numero: numero,
-            complemento: complemento,
             telefone: telefone,
+            email: email,
             metodoPagamento: metodoPagamento,
             troco: troco,
             itens: carrinho,
-            subtotal: formatarMoeda(subtotalValue),
-            total: formatarMoeda(totalPedido),
+            total: formatarMoeda(total),
             status: "Pendente",
             timestamp: firebase.firestore.FieldValue.serverTimestamp()
         }).then((docRef) => {
@@ -112,34 +94,5 @@ document.addEventListener('DOMContentLoaded', function() {
         }).catch((error) => {
             console.error("Erro ao salvar pedido: ", error);
         });
-    }
-
-    // Função para consultar o CEP automaticamente ao digitar
-    async function consultarCep() {
-        const cep = document.getElementById('cep').value;
-        const resultadoCep = document.getElementById('resultado-cep');
-        const cepFormatado = cep.replace(/\D/g, '');
-
-        if (cepFormatado.length === 8) {
-            try {
-                const response = await fetch(`https://viacep.com.br/ws/${cepFormatado}/json/`);
-                const data = await response.json();
-
-                if (data.erro) {
-                    resultadoCep.innerText = 'CEP não encontrado. Por favor, verifique o CEP informado.';
-                } else {
-                    resultadoCep.innerText = `Endereço: ${data.logradouro}, ${data.bairro} - ${data.localidade}/${data.uf}`;
-                    document.getElementById('endereco').value = data.logradouro;
-                }
-            } catch (error) {
-                console.error('Erro ao consultar CEP:', error);
-                resultadoCep.innerText = 'Ocorreu um erro ao consultar o CEP. Por favor, tente novamente mais tarde.';
-            }
-        } else {
-            resultadoCep.innerText = '';
-        }
-    }
-
-    // Atualizar o resumo do pedido ao carregar a página
-    updateOrderSummary();
+    });
 });
